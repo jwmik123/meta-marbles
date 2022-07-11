de17e11ce899ca35111f367828f78d5a68bddfec
Joël Mik <joel@megosu.com>
2022-07-11 16:31:45 +0200

    swapped planets

--- src/components/Masthead.js
+++ src/components/Masthead.js
@@ -14,7 +14,7 @@ const Masthead = () => {
         data-aos-duration="500"
         className="flex flex-col text-white text-6xl lg:text-[200px] font-display lg:leading-[160px]"
       >
-        Meta <span className="ml-8 lg:ml-28">Marbles.</span>
+        Meta <span className="ml-8 lg:ml-28 -z-20">Marbles.</span>
       </h1>
       <button className="absolute bottom-0 right-0 bg-white py-2 px-6 text-md md:text-lg rounded-sm">
         <strong>Mint</strong>: 16th of August 2022
@@ -23,16 +23,20 @@ const Masthead = () => {
       <div
         data-scroll
         data-scroll-speed="2"
-        className="absolute right-0 top-2 -z-10"
+        className="absolute left-12 md:left-4 bottom-2 -z-30 md:-z-10"
       >
-        <img src={marble1} alt="meta marble" width="400px" height="400px" />
+        <img src={marble1} alt="meta marble" className="w-72 lg:w-[400px]" />
       </div>
       <div
         data-scroll
         data-scroll-speed="4"
-        className="absolute -left-12 bottom-2 -z-10"
+        className="absolute right-0 top-2 -z-30"
       >
-        <img src={marble2} alt="meta marble" width="400px" height="400px" />
+        <img
+          src={marble2}
+          alt="meta marble"
+          className="hidden md:block w-72 lg:w-[500px]"
+        />
       </div>
     </section>
   );
