10849b55b6f67ca5161088d69cab2fbc3b5bb548
Joël Mik <joel@megosu.com>
2022-07-11 17:48:14 +0200

    kinda responsive about section

--- src/components/About.js
+++ src/components/About.js
@@ -2,11 +2,11 @@ import blackhole from "../assets/blackhole.jpeg";
 
 export const About = () => {
   return (
-    <section className="min-h-[800px] mt-80 text-white">
+    <section className="min-h-[800px] px-12 lg:px-44 my-80 text-white">
       <h1 className="text-6xl text-center font-display mb-12">
         So, what are Meta Marbles?
       </h1>
-      <div className="flex gap-12 flex-col md:flex-row">
+      <div className="flex gap-12 flex-col lg:flex-row">
         <div className="flex flex-col justify-between text-lg">
           <p className="">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque
@@ -28,7 +28,11 @@ export const About = () => {
           </p>
         </div>
         <div>
-          <img className="w-full " src={blackhole} alt="meta marble" />
+          <img
+            className="w-full h-full overflow-hidden"
+            src={blackhole}
+            alt="meta marble"
+          />
         </div>
       </div>
     </section>
