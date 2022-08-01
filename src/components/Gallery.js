132e5ab4a32e6b3994768c24e398213698aea8d3
Joël Mik <joel@megosu.com>
2022-08-01 17:46:32 +0200

    Next updte

---
+++ src/components/Gallery.js
@@ -0,0 +1,3 @@
+export const Gallery = () => {};
+
+export default Gallery;
--- src/components/Team.js
+++ src/components/Team.js
@@ -31,19 +31,39 @@ export const Team = () => {
                 Small text about how creative and passionate this guy is about
                 the project.
               </p>
+              <a href="#" className="flex flex-row items-center">
+                <span className="flex flex-row font-medium">LinkedIn</span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="bi bi-chevron-right"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
+                  />
+                </svg>
+              </a>
             </div>
           </div>
         </div>
         <div className="team-item flex items-center p-4 md:p-8 lg:p-10">
           <h3 className="text-4xl text-black font-medium text-center">
-            Follow us on{" "}
-            <span className="text-purple-700 underline">Discord</span>
+            Join us on <a className="text-purple-700 underline">Discord</a>
           </h3>
         </div>
         <div className="team-item flex items-center p-4 md:p-8 lg:p-10">
           <h3 className="text-4xl text-black font-medium text-center">
             Follow us on{" "}
-            <span className="text-blue-500 underline">Twitter</span>
+            <a
+              href="https://twitter.com/meta-marbles"
+              className="text-blue-500 underline"
+            >
+              Twitter
+            </a>
           </h3>
         </div>
         <div className="md:col-span-2 team-item">
@@ -62,6 +82,22 @@ export const Team = () => {
                 Small text about how creative and passionate this guy is about
                 the project.
               </p>
+              <a href="#" className="flex flex-row items-center">
+                <span className="flex flex-row font-medium">LinkedIn</span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="bi bi-chevron-right"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
+                  />
+                </svg>
+              </a>
             </div>
           </div>
         </div>
