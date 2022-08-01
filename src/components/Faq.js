89795fae4b2d926130fcd1dcaeb11e2136f2e79f
Joël Mik <joel@megosu.com>
2022-08-02 00:59:08 +0200

    added content to FAQ

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -4,24 +4,39 @@ const FaqComponent = () => {
   const data = {
     rows: [
       {
-        title: "What is an NFT?",
-        content: "An NFT is an...",
+        title: <h3 className="text-2xl font-medium">What is an NFT?</h3>,
+        content:
+          "An NFT is a digital asset that represents real-world objects like art, music, in-game items and videos. They are bought and sold online, frequently with cryptocurrency, and they are generally encoded with the same underlying software as many cryptos.",
       },
       {
-        title: "What is an NFT?",
-        content: "An NFT is an...",
+        title: (
+          <h3 className="text-2xl font-medium">How can I buy a Meta Marble?</h3>
+        ),
+        content:
+          "You will be able to mint a Meta Marble through the link in our\n" +
+          "Discord or clicking on the Mint button on this page.",
       },
       {
-        title: "What is an NFT?",
-        content: "An NFT is an...",
+        title: (
+          <h3 className="text-2xl font-medium">
+            How do I get $SOL in my wallet?
+          </h3>
+        ),
+        content:
+          "We would love to refer you to the Magic Eden Playbook. Here you'll find exactly how to create and use your wallet.\n" +
+          "Magic Eden Playbook: https://contenthub.magiceden.io/playbook",
       },
       {
-        title: "What is an NFT?",
-        content: "An NFT is an...",
+        title: (
+          <h3 className="text-2xl font-medium">Will there be a whitelist?</h3>
+        ),
+        content: "No. Fair game for everyone",
       },
       {
-        title: "What is an NFT?",
-        content: "An NFT is an...",
+        title: (
+          <h3 className="text-2xl font-medium">What will be the mint price?</h3>
+        ),
+        content: "Mint price will be 1.5 SOL.",
       },
     ],
   };
