0236067fde37bb575a75c46b2455abbf9ea180b2
Joël Mik <joel@megosu.com>
2022-08-09 17:36:38 +0200

    Added game information to the Roadmpa section

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -24,7 +24,10 @@ const FaqComponent = () => {
         ),
         content:
           "We would love to refer you to the Magic Eden Playbook. Here you'll find exactly how to create and use your wallet.\n" +
-          "Magic Eden Playbook: https://contenthub.magiceden.io/playbook",
+          'Magic Eden Playbook: <a href=\'https://contenthub.magiceden.io/playbook\'>https://contenthub.magiceden.io/playbook <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-in-up-right" viewBox="0 0 16 16">\n' +
+          '  <path fill-rule="evenodd" d="M6.364 13.5a.5.5 0 0 0 .5.5H13.5a1.5 1.5 0 0 0 1.5-1.5v-10A1.5 1.5 0 0 0 13.5 1h-10A1.5 1.5 0 0 0 2 2.5v6.636a.5.5 0 1 0 1 0V2.5a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 .5.5v10a.5.5 0 0 1-.5.5H6.864a.5.5 0 0 0-.5.5z"/>\n' +
+          '  <path fill-rule="evenodd" d="M11 5.5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793l-8.147 8.146a.5.5 0 0 0 .708.708L10 6.707V10.5a.5.5 0 0 0 1 0v-5z"/>\n' +
+          "</svg></a>",
       },
       {
         title: (
@@ -51,17 +54,23 @@ const FaqComponent = () => {
 
   const config = {
     animate: true,
-    arrowIcon: "v",
+    arrowIcon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="16"
+        height="16"
+        fill="currentColor"
+        className="bi bi-caret-down-fill"
+        viewBox="0 0 16 16"
+      >
+        <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z" />
+      </svg>
+    ),
     tabFocus: false,
   };
 
   return (
-    <section
-      data-scroll
-      data-scroll-delay="1"
-      data-scroll-speed="2"
-      className="min-h-[400px] px-12 lg:px-44 my-40 text-white"
-    >
+    <section className="min-h-[400px] px-12 lg:px-44 my-40 text-white">
       <h1 className="font-oswald text-6xl text-center mb-12">
         Frequently Asked Questions.
       </h1>
--- src/components/Roadmap.js
+++ src/components/Roadmap.js
@@ -3,37 +3,60 @@ import { Timeline, Event } from "react-timeline-scribble";
 
 const Roadmap = () => {
   return (
-    <section
-      data-scroll
-      data-scroll-delay="1"
-      data-scroll-speed="2"
-      className="min-h-[400px] py-6 md:py-12 px-12 lg:px-44 my-40 bg-white"
-    >
+    <section className="min-h-[400px] py-6 md:py-12 px-12 lg:px-44 my-40 bg-white">
       <h1 className="font-oswald text-6xl text-center mb-12">Roadmap.</h1>
-      <Fragment>
-        <Timeline>
-          <Event
-            interval={"Phase 1"}
-            title={"Mint of collection"}
-            subtitle={"1st of September 2022"}
-          ></Event>
-          <Event
-            interval={"Phase 2"}
-            title={"Starting the creating of the game"}
-            subtitle={"If 75% of the collection is sold"}
-          ></Event>
-          <Event
-            interval={"Phase 3"}
-            title={"Launch of the game"}
-            subtitle={"Season 1: Milky Way"}
-          >
-            <ul>
-              <li>Everyday a Cup Of The Day at 7PM</li>
-              <li>1v1 marbles against other holders</li>
-            </ul>
-          </Event>
-        </Timeline>
-      </Fragment>
+      <div className="flex flex-col md:flex-row justify-between">
+        <Fragment>
+          <Timeline>
+            <Event
+              interval={"Phase 1"}
+              title={"Mint of collection"}
+              subtitle={"1st of September 2022"}
+            ></Event>
+            <Event
+              interval={"Phase 2"}
+              title={"Starting the creating of the game"}
+              subtitle={"If 75% of the collection is sold"}
+            ></Event>
+            <Event
+              interval={"Phase 3"}
+              title={"Launch of the game"}
+              subtitle={"Season 1: Milky Way"}
+            >
+              <ul>
+                <li>Every monday a Cup Of The Week at 7PM</li>
+                <li>1v1 marbles against other holders at anytime</li>
+              </ul>
+            </Event>
+          </Timeline>
+        </Fragment>
+        <div className="w-full md:w-1/3 mt-16 md:mt-0">
+          <h2 className="text-xl underline mb-2">The game</h2>
+          <p>
+            The game will be called Meta Marbles and at the start will only be
+            playable at our decentralized platform.
+          </p>
+          <p>
+            The first season is called "Milky Way" and consists of 3 tracks.
+          </p>
+          <p>
+            The winners of the Cup Of The Week(COTW) will be shown on a
+            leaderboard in the Meta Marbles DAO.
+          </p>
+          <p>
+            At the end of the season, the holder with the most $tokens will win
+            the NFT trophy.
+          </p>
+          <h2 className="text-xl underline mt-4 mb-2">1v1</h2>
+          <p>
+            Every holder is able to challenge another holder to play a 1v1
+            marbles race. The winner takes home the Meta Marble of the loser.
+          </p>
+          <p>
+            <strong>Are you brave enough?</strong>
+          </p>
+        </div>
+      </div>
     </section>
   );
 };
